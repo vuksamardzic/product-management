dc65d43e6a0b8ad81be23a7bb75c15debca76242
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-03 23:35:07 +0100

    IProduct interface added

---
+++ src/app/core/interfaces/product.interface.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    code: string;
+    description: string;
+    available: number;
+    price: number;
+    rating: number;
+    imageUrl: string;
+}
