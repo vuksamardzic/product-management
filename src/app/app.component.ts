83b702dec4538125b877c242aee2002a3d305606
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-04 00:03:23 +0100

    minor update and importing in main module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductService } from './shared/services/product.service';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'pm';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http"
 
-import { AppRoutingModule } from './app-routing.module';
-
+import { AppRoutingModule } from './core/modules/app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { ProductListComponent } from './core/components/product-list/product-list.component';
+import { ConvertToSpacePipe } from './core/pipes/convert-to-space.pipe';
+import { StarComponent } from './shared/components/star/star.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductListComponent,
+    ConvertToSpacePipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
