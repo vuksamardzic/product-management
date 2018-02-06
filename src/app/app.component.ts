e0040fc7d2c2b96700a60b21ce6ae44e721bf930
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-06 17:45:43 +0100

    app.module update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent { 
+  constructor(private router: Router) { }
+
+  logOut(): void {
+    this.router.navigateByUrl('');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/components/home/home.component';
 import { ProductModule } from './core/modules/product/product.module';
+import { UserModule } from './core/modules/user/user.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ProductModule } from './core/modules/product/product.module';
   imports: [
     BrowserModule,
     ProductModule,
+    UserModule,
     AppRoutingModule
   ],
   providers: [],
