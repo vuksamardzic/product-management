8227ce7592804c5ee464696b99fe0e4a65573a1a
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-20 22:53:25 +0100

    enable loading while prefetching data using router events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationError, NavigationCancel, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { 
-  constructor(private router: Router) { }
+export class AppComponent {
+  loading: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    } else if (
+      routerEvent instanceof NavigationError ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationEnd
+    ) {
+      this.loading = false;
+    }
+  }
 
   logOut(): void {
     this.router.navigateByUrl('');
